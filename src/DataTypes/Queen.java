a1f4fb58771e2964edd1e097f38ff8171f2c3d13
Marek <fechmarek5@gmail.com>
2022-11-29 15:27:29 +0100

    Vloženie DataTypov (+ celý Queen), Interfacov a Enumerácie

---
+++ src/DataTypes/Queen.java
@@ -0,0 +1,13 @@
+package DataTypes;
+
+public class Queen {
+    private int points;
+
+    public Queen(int points){
+        this.points = points;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+}
